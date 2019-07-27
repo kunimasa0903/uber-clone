42e759b47e702694596b3aa89827161793e520d7
kunimasandbox <str.kunimasa@hands-on-campus.com>
2019-07-27 13:44:39 +0000

    nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    "@/plugins/firebase"
   ],
   /*
   ** Nuxt.js dev-modules
@@ -57,7 +58,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,202 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <v-container>
+      <v-btn text @click="get" color="primary">get</v-btn>
+      {{hoge}}
+        <v-btn @click="getLocation" color="info" fab :loading="loading">
+          <v-icon>mdi-crosshairs-gps</v-icon>
+        </v-btn>
+      lat:{{location.lat}}
+      lng:{{location.lng}}
+      <v-btn fab color="secondary" @click="searchTaxi">
+        <v-icon>mdi-taxi</v-icon>
+      </v-btn>
+    </v-container>
+
+      <GmapMap
+        :center="{lat:location.lat, lng:location.lng}"
+        :zoom="18"
+        style="width:100vw;height:100vh"
+      >
+        <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="false"
+          :icon="icon"
+          @click="toggleInfo(m,index)"
+        />
+      </GmapMap>
+      <v-dialog v-model="infoOpened">
+        <v-card light>
+          <v-container>
+            <v-layout column wrap justify-center align-center>
+              <h2 class="pa-3">Hey, TAXI!</h2>
+              <v-icon size="56">mdi-taxi</v-icon>
+              <v-btn color="primary" rounded ><v-icon left>mdi-human-handsup</v-icon>Request</v-btn>
+              <v-btn class="mt-4" color="success" text><v-icon left>mdi-phone</v-icon>Call</v-btn>
+            </v-layout>
+
+          </v-container>
+        </v-card>
+      </v-dialog>
+      <v-btn color="info" @click="move" v-show="location.lat" fab><v-icon>mdi-walk</v-icon></v-btn>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDpe7EEYDCVnfH9IXJJmA2x_zjJryJF11s',
+  },
+  installComponents: true
+})
+function boundingBoxCoordinates(center, radius) {
+  const KM_PER_DEGREE_LATITUDE = 110.574;
+  const latDegrees = radius / KM_PER_DEGREE_LATITUDE;
+  const latitudeNorth = Math.min(90, center.lat + latDegrees);
+  const latitudeSouth = Math.max(-90, center.lat - latDegrees);
+  // calculate longitude based on current latitude
+  const longDegsNorth = metersToLongitudeDegrees(radius, latitudeNorth);
+  const longDegsSouth = metersToLongitudeDegrees(radius, latitudeSouth);
+  const longDegs = Math.max(longDegsNorth, longDegsSouth);
+  return {
+    swCorner: { // bottom-left (SW corner)
+      latitude: latitudeSouth,
+      longitude: wrapLongitude(center.lng - longDegs),
+    },
+    neCorner: { // top-right (NE corner)
+      latitude: latitudeNorth,
+      longitude: wrapLongitude(center.lng + longDegs),
+    },
+  };
+
+  function metersToLongitudeDegrees(distance, latitude) {
+    const EARTH_EQ_RADIUS = 6378137.0;
+    // this is a super, fancy magic number that the GeoFire lib can explain (maybe)
+    const E2 = 0.00669447819799;
+    const EPSILON = 1e-12;
+    const radians = degreesToRadians(latitude);
+    const num = Math.cos(radians) * EARTH_EQ_RADIUS * Math.PI / 180;
+    const denom = 1 / Math.sqrt(1 - E2 * Math.sin(radians) * Math.sin(radians));
+    const deltaDeg = num * denom;
+    if (deltaDeg < EPSILON) {
+      return distance > 0 ? 360 : 0;
+    }
+    // else
+    return Math.min(360, distance / deltaDeg);
+
+    function degreesToRadians(degrees) {
+      return (degrees * Math.PI) / 180;
+    }
+  }
+
+  function wrapLongitude(longitude) {
+    if (longitude <= 180 && longitude >= -180) {
+      return longitude;
+    }
+    const adjusted = longitude + 180;
+    if (adjusted > 0) {
+      return (adjusted % 360) - 180;
+    }
+    // else
+    return 180 - (-adjusted % 360);
+  }
+}
+
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data(){
+    return {
+      hoge:null,
+      location:{},
+      loading:false,
+      mapWidth:window.innerWidth*0.8,
+      mapHeight:window.innerHeight*0.5,
+      icon: {
+        url: require('@/static/taxi.png'),
+        size: {width: 30, height: 30, f: 'px', b: 'px'},
+        scaledSize: {width: 30, height: 30, f: 'px', b: 'px'}
+      },
+      infoOpened: false,
+      additionalMarkers:[]
+    }
+  },
+  computed:{
+    markers(){
+      return [{
+        position:{
+          lat:this.location.lat,
+          lng:this.location.lng
+        },
+        title:"you are here."
+      },...this.additionalMarkers]
+    }
+  },
+  methods:{
+    uid(){
+      return this.$auth.currentUser.uid;
+    },
+    get(){
+      const uid = this.uid()
+      console.log({uid})
+      this.hoge=null;
+      this.$firestore.doc(`users/${uid}`)
+        .get()
+        .then(doc=>{
+          this.hoge = doc.data()
+      })
+    },
+    getLocation(){
+      this.loading = true;
+      navigator.geolocation.getCurrentPosition(location=>{
+        this.location = {
+          lat:location.coords.latitude,
+          lng:location.coords.longitude,
+          timestamp:location.timestamp
+        }
+        this.loading=false
+      },err=>{
+        console.error(err)
+        this.loading=false
+      });
+    },
+    move(){
+      if(!this.location.lat || !this.location.lng) return;
+      this.location.lat += (Math.random() - Math.random())/10000
+      this.location.lng += (Math.random() - Math.random())/10000
+    },
+    toggleInfo(m,i){
+      this.infoOpened=!this.infoOpened
+    },
+    searchTaxi(){
+      const radius = 0.0001;
+      const ne = new this.$GeoPoint(this.location.lat+radius,this.location.lng+radius)
+      const sw = new this.$GeoPoint(this.location.lat-radius,this.location.lng-radius)
+      this.$firestore.collection('taxies')
+        .where('geoPoint','>=',sw)
+        .where('geoPoint','<=',ne)
+        .limit(10)
+        .get()
+        .then(query=>{　// [<firestore query>,<firebase query>]
+          const taxies = query.docs.map(doc=>doc.data()) // {name:"...",...}
+          console.table(taxies)
+        })
+    }
+  },
+  watch:{
+    location(location){
+      const geoPoint = new this.$GeoPoint(location.lat,location.lng)
+      this.$firestore.doc(`users/${this.uid()}`)
+        .update({geoPoint})
+        .then(()=>{
+          console.info("updated",geoPoint)
+        })
+    }
   }
 }
 </script>
